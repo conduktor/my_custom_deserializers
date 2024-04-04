05fbdb0a762c5a110d46ea1a34f416b5ec8bbdf8
Quentin Boileau <qboileau@conduktor.io>
2024-04-04 11:45:51 +0200

    Update README and MyConfigurableDeserializer

--- src/main/scala/io/example/conduktor/custom/deserializers/MyConfigurableDeserializer.scala
+++ src/main/scala/io/example/conduktor/custom/deserializers/MyConfigurableDeserializer.scala
@@ -10,16 +10,28 @@ case object MyConfigurableDeserializerException
       "ConfigurableDeserializer fail when its `::configure` method is called without `output` property"
     )
 
+sealed trait Output
+final case class Constant(value: String)             extends Output
+final case class Config(config: util.Map[String, _]) extends Output
+final case object Passthrough                        extends Output
+final case object Unconfigured                       extends Output
+
 final class MyConfigurableDeserializer extends Deserializer[Any] {
 
-  var output: Any = "unconfigured"
+  var output: Output = Unconfigured
 
-  override def deserialize(topic: String, data: Array[Byte]): Any = output
+  override def deserialize(topic: String, data: Array[Byte]): Any = output match {
+    case Constant(value) => value
+    case Config(config)  => config
+    case Passthrough     => data
+    case Unconfigured    => throw MyConfigurableDeserializerException
+  }
 
   override def configure(configs: util.Map[String, _], isKey: Boolean): Unit =
     configs.asScala.get("output").map(_.asInstanceOf[String]) match {
-      case Some("config") => output = configs
-      case Some(value)    => output = value
-      case None           => throw MyConfigurableDeserializerException
+      case Some("config")      => output = Config(configs)
+      case Some("passthrough") => output = Passthrough
+      case Some(value)         => output = Constant(value)
+      case None                => throw MyConfigurableDeserializerException
     }
 }
